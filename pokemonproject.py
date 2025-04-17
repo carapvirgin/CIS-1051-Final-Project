135adf0a3a13bbab7481c4266cf47f0c7b133c95
Christopher Mejia-Gonzales <tuo45455@temple.edu>
2025-04-17 12:28:39 -0400

    Add files via upload

---
+++ pokemonproject.py
@@ -0,0 +1,43 @@
+import time
+import numpy as np
+import sys
+
+
+def delay_rint(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+class Pokemon:
+    def __init__(self, name, types, moves, EVs, health):
+        self.name = name
+        self.types - types
+        self.moves = moves
+        self.attack = EVs['ATTACK']
+        self.defense = EVs['DEFENSE']
+        self.bars = 20 
+    
+    def fight(self, Pokemon2):
+        print("-------POKEMON BATTLE------")
+        print(f"\n{self.name}")
+        print("TYPE/", self.types)
+        print("ATTACK/", self.attack)
+        print("DEFENSE/", self.defense)
+        print("LVL/", 3*(1 + np.mean([self.attack, self.defense])))
+        print("\nVS")
+        print("-------POKEMON BATTLE------")
+        print(f"\n{Pokemon2.name}")
+        print("TYPE/", Pokemon2.types)
+        print("ATTACK/", Pokemon2.attack)
+        print("DEFENSE/", Pokemon2.defense)
+        print("LVL/", 3*(1 + np.mean([Pokemon2.attack, Pokemon2.defense])))
+        print("\nVS")
+        
+        time.sleep(2)
+        
+        #Pokemon Types
+        types = ['Fire', 'Water', 'Grass', 'Rock', 'Ground', 'Ice', 'Psychic', 'Ghost', 'Fighting', 'Steel', 'Poision', 'Fairy', 'Dark']
+        if k in enumerate(types):
+            if self.types == k:
+                
