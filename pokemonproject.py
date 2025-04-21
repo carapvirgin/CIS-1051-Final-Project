d224528cf7a293aa10838666210f621c1cc9e855
Cara Virgin <163333798+carapvirgin@users.noreply.github.com>
2025-04-21 11:20:35 -0400

    Update pokemonproject.py
    4/21 11 AM changes

--- pokemonproject.py
+++ pokemonproject.py
@@ -1,13 +1,40 @@
 import time
 import numpy as np
 import sys
+import pyttsx3
+import requests
+import json
+import os
 
+#Google voice to text to narrate
+voice = pyttsx3.init()
+voice.say("Pikachu used Thunderbolt!")
+voice.runAndWait()
 
+#delay text printing
 def delay_rint(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.05)
+
+#downloading and loading the full pokedex
+def get_pokemon_data(pokemon_id):
+    url = f"https://classic.pokepc.net/apps/pokedex/{pokemon_id}/"
+    respond  = requests.get(url)
+    if respond.status_code == 200:
+        data = respond.json()
+        return {
+            "id": data["id"].
+            "name": data["name"].capitalize(),
+            "types": [t["type"]["name"].capitalize() for t in data["types"]],
+            "stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]},
+            "abilities": [a["ability"]["name"] for a in data["abilities"]],
+            "height": data["height"],
+            "weight": data["weight"]
+        }
+    return None
+
 
 class Pokemon:
     def __init__(self, name, types, moves, EVs, health):
