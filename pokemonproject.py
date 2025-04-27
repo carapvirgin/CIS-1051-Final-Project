6dd717d46dc8193b2170688eacd69ce60f1247f3
Cara Virgin <163333798+carapvirgin@users.noreply.github.com>
2025-04-27 12:01:39 -0400

    Update pokemonproject.py

--- pokemonproject.py
+++ pokemonproject.py
@@ -40,7 +40,25 @@ for r in rows:
     colms = row.find_all("td")
     if len(colms) < 6:
         continue
-        
+
+    number = colms[0].text.strip()
+    name = colms[3].text.strip()
+    type1 = colms[4].img["alt"]
+    type2 = colms[5].img["alt"]
+
+    pokedex.append({
+        "number": number,
+        "name": name,
+        "types": [type1] if not type2 else [type1,type2]
+    })
+
+with open(filename,"w") as f:
+    json.dump(pokedex, f, indent = 2)
+print(f"Saved {len(pokedex)} Pokémon to {filename}")
+
+def load_serebii_pokedex(filename="serebii_pokedex.json")
+    with open(filename, "r") as f:
+        return json.load(f)
 
 class Pokemon:
     def __init__(self, name, types, moves, EVs, health):
@@ -73,4 +91,25 @@ class Pokemon:
         types = ['Fire', 'Water', 'Grass', 'Rock', 'Ground', 'Ice', 'Psychic', 'Ghost', 'Fighting', 'Steel', 'Poision', 'Fairy', 'Dark']
         if k in enumerate(types):
             if self.types == k:
-                
+ def create_random_pokedex(pokedex):
+     entry = random.choice(pokedex)
+      name = entry['name']
+      types = entry['types']
+      moves = []
+      EVs = {
+         'ATTACK': random.randit(40,100),
+            'DEFENSE': random.randit(40,100)
+      }
+       health = random.randit(50,100)
+       return Pokemon(name,types,moves,EVs, health)
+if __name__ == "__main__":
+    scrape_serebii_pokedex()
+    pokedex = load_serebii_pokedex()
+
+    print("\nCreating two random Pokémon...\n")
+    p1 = create_random_pokemon(pokedex)
+    p2 = create_random_pokemon(pokedex)
+
+    p1.fight(p2)
+
+         
